d2c64cf577e70d92f1a0074e7eeb2702987f7a2b
917SusciucAnastasia <susciuc.anastasia@gmail.com>
2023-03-17 19:26:39 +0200

    Add lab2

---
+++ maven01/src/test/java/ssvv/example/AppTest.java
@@ -0,0 +1,34 @@
+package ssvv.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import ssvv.example.domain.Student;
+import ssvv.example.repository.StudentRepository;
+import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.Validator;
+
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testAddStudent()
+    {
+        Validator<Student> studentValidator = new StudentValidator();
+        StudentRepository studentRepository = new StudentRepository(studentValidator);
+
+        Student student = new Student("1234", "Ana", 876);
+        studentRepository.save(student);
+
+        assertEquals(studentRepository.findOne("1234").getID(), "1234");
+        assertEquals(studentRepository.findOne("1234").getNume(), "Ana");
+        assertEquals(studentRepository.findOne("1234").getGrupa(), 876);
+    }
+}
