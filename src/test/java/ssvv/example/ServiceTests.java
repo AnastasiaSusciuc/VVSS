6863f2a9de80c134e299235e2560ce59b52720aa
917SusciucAnastasia <susciuc.anastasia@gmail.com>
2023-04-04 17:33:13 +0300

    add lab 3 v2

---
+++ src/test/java/ssvv/example/ServiceTests.java
@@ -0,0 +1,51 @@
+package ssvv.example;
+
+import org.junit.Test;
+import ssvv.example.repository.NotaXMLRepository;
+import ssvv.example.repository.StudentXMLRepository;
+import ssvv.example.repository.TemaXMLRepository;
+import ssvv.example.service.Service;
+import ssvv.example.validation.NotaValidator;
+import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.TemaValidator;
+
+import static org.junit.Assert.assertEquals;
+
+public class ServiceTests {
+    @Test
+    public void TestCase1() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        try {
+            service.saveTema("14", "", 2,5);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "Descriere invalida!");
+        }
+    }
+
+    @Test
+    public void TestCase2() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        assertEquals(service.saveTema("15", "", 2,5), 1);
+        assertEquals(service.findAllTeme().iterator().next().getID(), "1");
+    }
+}
