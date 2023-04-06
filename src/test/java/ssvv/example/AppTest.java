8973ed25b029e2aeaaf225f3b4d1d51515203957
917SusciucAnastasia <susciuc.anastasia@gmail.com>
2023-04-06 13:50:08 +0300

    add bva

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -99,4 +99,221 @@ public class AppTest
 
         studentXMLRepository.delete("1");
     }
+
+    @Test
+    public void testAddStudentTH2BVA1()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        int res = service.saveStudent(null, "Maria",111);
+        assertEquals(1, res);
+    }
+
+    @Test
+    public void testAddStudentTH2BVA2()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        int res = service.saveStudent("", "Maria",111);
+        assertEquals(1, res);
+    }
+
+    @Test
+    public void testAddStudentTH2BVA3()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        int res = service.saveStudent("a", "Maria",111);
+        assertEquals(0, res);
+        res = service.saveStudent("aa", "Maria",111);
+        assertEquals(0, res);
+        String a255 = new String(new char[255]).replace('\0', 'a');
+        String a254 = new String(new char[254]).replace('\0', 'a');
+
+        res = service.saveStudent(a254, "Maria",111);
+        assertEquals(0, res);
+
+        res = service.saveStudent(a255, "Maria",111);
+        assertEquals(0, res);
+
+        studentXMLRepository.delete("a");
+        studentXMLRepository.delete("aa");
+        studentXMLRepository.delete(a254);
+        studentXMLRepository.delete(a255);
+    }
+
+    @Test
+    public void testAddStudentTH2BVA4()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        int res = service.saveStudent("1", null,111);
+        assertEquals(1, res);
+    }
+
+    @Test
+    public void testAddStudentTH2BVA5()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        int res = service.saveStudent("1", "",111);
+        assertEquals(1, res);
+    }
+
+    @Test
+    public void testAddStudentTH2BVA6()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        int res = service.saveStudent("1", "a",111);
+        assertEquals(0, res);
+        res = service.saveStudent("2", "aa",111);
+        assertEquals(0, res);
+        String a255 = new String(new char[255]).replace('\0', 'a');
+        String a254 = new String(new char[254]).replace('\0', 'a');
+
+        res = service.saveStudent("3", a254,111);
+        assertEquals(0, res);
+
+        res = service.saveStudent("4", a255,111);
+        assertEquals(0, res);
+
+        studentXMLRepository.delete("1");
+        studentXMLRepository.delete("2");
+        studentXMLRepository.delete("3");
+        studentXMLRepository.delete("4");
+    }
+
+    @Test
+    public void testAddStudentTH2BVA7()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        int res = service.saveStudent("1", "a",110);
+        assertEquals(1, res);
+        res = service.saveStudent("2", "aa",111);
+        assertEquals(0, res);
+        studentXMLRepository.delete("1");
+        studentXMLRepository.delete("2");
+    }
+
+    @Test
+    public void testAddStudentTH2BVA8()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        int res = service.saveStudent("1", "a",110);
+        assertEquals(1, res);
+        res = service.saveStudent("2", "aa",111);
+        assertEquals(0, res);
+        res = service.saveStudent("3", "aa",112);
+        assertEquals(0, res);
+        studentXMLRepository.delete("1");
+        studentXMLRepository.delete("2");
+        studentXMLRepository.delete("3");
+        res = service.saveStudent("4", "aa",936);
+        assertEquals(0, res);
+        res = service.saveStudent("5", "aa",937);
+        assertEquals(0, res);
+        res = service.saveStudent("6", "aa",938);
+        assertEquals(1, res);
+        studentXMLRepository.delete("4");
+        studentXMLRepository.delete("5");
+        studentXMLRepository.delete("6");
+    }
+
+    @Test
+    public void testAddStudentTH2BVA9()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "studenti.xml";
+        String filenameTema = "teme.xml";
+        String filenameNota = "note.xml";
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+        Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+
+        int res = service.saveStudent("5", "aa",938);
+        assertEquals(1, res);
+        res = service.saveStudent("6", "aa",937);
+        assertEquals(0, res);
+        studentXMLRepository.delete("5");
+        studentXMLRepository.delete("6");
+    }
+
 }
